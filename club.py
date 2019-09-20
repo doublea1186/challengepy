a89ac23a9917472f4190db96c5f39b73cb6dcf02
Ahmed Ahmed <doublea1186@gmail.com>
2019-09-20 16:31:04 -0400

    minor update

--- club.py
+++ club.py
@@ -1,8 +1,9 @@
 class Club:
-    def __init__(self, name, description, tags):
+    def __init__(self, name, description, tags, likes):
         self.name = name
         self.tags = tags
         self.description = description
+        self.likes = likes
 
     def set_name(self, name):
         self.name = name
@@ -13,6 +14,9 @@ class Club:
     def set_description(self, description):
         self.description = description
 
+    def set_likes(self, likes):
+        self.likes = likes
+
     def get_name(self):
         return self.name
 
@@ -21,4 +25,7 @@ class Club:
 
     def get_description(self):
         return self.description
+
+    def get_likes(self):
+        return self.likes
 
